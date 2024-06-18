444d23b76727cf807bfd5e73704ab795607c7e6a
= <=>
2024-06-18 10:19:55 -0300

    first commit

--- Parking/SaveLocation.swift
+++ Parking/SaveLocation.swift
@@ -11,55 +11,96 @@ import CoreLocation
 
 struct saveLocation: View {
     @Environment(\.colorScheme) var colorScheme
+    @Environment(\.colorScheme) var switchColors
     @StateObject private var locationManager = LocationManager()
     @State private var carLocation: CLLocation?
+    @State private var showAlert = false
+    @State private var referenceText: String = ""
+    @State private var offset: CGFloat = 0
     var carIcon: String
-
+    
     var body: some View {
         NavigationView {
-            VStack {
-                if let carImage = UIImage(named: carIcon) {
-                    Image(uiImage: carImage)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 200, height: 200)
-                        .padding()
-                }
-                
-                Spacer()
-                
-                // TableView
-                if let userLocation = locationManager.userLocation {
-                    List {
-                        Text("Latitude: \(userLocation.coordinate.latitude)")
-                        Text("Longitude: \(userLocation.coordinate.longitude)")
+            ScrollView {
+                VStack {
+                    if let carImage = UIImage(named: carIcon) {
+                        Image(uiImage: carImage)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 200, height: 200)
+                            .padding()
                     }
-                    .listStyle(.insetGrouped)
-                    .background(Color.clear)
-                    .scrollContentBackground(.hidden)
-                    .scrollDisabled(true)
-                }
-                
-                Button(action: {
-                    handleButtonAction()
-                }) {
-                    Text(carLocation != nil ? "Open Maps" : "Save car location")
-                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+                    if let userLocation = locationManager.userLocation {
+                        VStack(alignment: .leading, spacing: 10) {
+                            HStack {
+                                Text("Reference:")
+                                TextField("Enter reference", text: $referenceText)
+                                    .textFieldStyle(PlainTextFieldStyle())
+                                    .background(Color.clear)
+                            }
+                            Text("Latitude: \(userLocation.coordinate.latitude)")
+                            Text("Longitude: \(userLocation.coordinate.longitude)")
+                        }
                         .padding()
-                        .background(Color.blue)
+                        .background(colorsList)
                         .cornerRadius(10)
+                        .padding()
+                    }
+                    
+                    Button(action: {
+                        handleButtonAction()
+                    }) {
+                        Text(carLocation != nil ? "Open Maps" : "Save car location")
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .alert(isPresented: $showAlert) {
+                        Alert(
+                            title: Text("Location Already Saved"),
+                            message: Text("You already have a saved location. Are you sure you want to save another location?"),
+                            primaryButton: .default(Text("Yes")) {
+                                saveCarLocation()
+                            },
+                            secondaryButton: .cancel()
+                        )
+                    }
+                    
+                    Spacer()
                 }
                 .padding()
-                .frame(maxWidth: .infinity)
-                
-                Spacer()
+                .offset(y: -offset)
+                .animation(.easeOut(duration: 0.16))
+                .onAppear {
+                    NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { (notification) in
+                        if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+                            self.offset = keyboardFrame.height / 2
+                        }
+                    }
+                    
+                    NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { (notification) in
+                        self.offset = 0
+                    }
+                }
+                .onDisappear {
+                    NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+                    NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+                }
             }
-            .navigationTitle("Save Car Location")
+            .navigationBarTitle("Save Car Location", displayMode: .large)
             .padding()
             .background(backgroundColor.ignoresSafeArea())
         }
     }
     
+    
+    //MARK: backgroundColor
     var backgroundColor: Color {
         switch colorScheme {
         case .light:
@@ -71,25 +112,61 @@ struct saveLocation: View {
         }
     }
     
+    var colorsList: Color {
+        switch switchColors {
+        case .light:
+            return Color.white
+        case .dark:
+            return Color(red: 51/255, green: 174/255, blue: 255/255)
+        @unknown default:
+            return Color.yellow.opacity(0.5)
+            
+        }
+    }
+    
+    //MARK: handleButtonAction
     private func handleButtonAction() {
+        let latitude = UserDefaults.standard.double(forKey: "latitude")
+        let longitude = UserDefaults.standard.double(forKey: "longitude")
+        
         if carLocation != nil {
             openMaps()
         } else {
-            saveCarLocation()
+            if latitude != 0 && longitude != 0 {
+                showAlert = true
+            } else {
+                saveCarLocation()
+            }
         }
     }
     
+    //MARK: saveCarLocation
     private func saveCarLocation() {
         if let location = locationManager.userLocation {
             carLocation = location
+            saveLocationToUserDefaults(location: location)
         }
     }
     
+    //MARK: saveLocationToUserDefaults
+    private func saveLocationToUserDefaults(location: CLLocation) {
+        let latitude = location.coordinate.latitude
+        let longitude = location.coordinate.longitude
+        let carModel = carIcon
+        let referenceText = referenceText
+        
+        UserDefaults.standard.set(latitude, forKey: "latitude")
+        UserDefaults.standard.set(longitude, forKey: "longitude")
+        UserDefaults.standard.set(carModel, forKey: "carModel")
+        UserDefaults.standard.set(referenceText, forKey: "referenceText")
+        
+    }
+    
+    //MARK: openMaps
     private func openMaps() {
         if let carLocation = carLocation {
             let coordinate = carLocation.coordinate
             let urlString = "http://maps.apple.com/?ll=\(coordinate.latitude),\(coordinate.longitude)&q=MyCar"
-            print("urlString: \(urlString)")
             if let url = URL(string: urlString) {
                 UIApplication.shared.open(url)
             }
--- Parking/carPicker.swift
+++ Parking/carPicker.swift
@@ -14,25 +14,59 @@ struct carPicker: View {
     @State private var selectedIcon: String = "car13"
     @State private var angle: Angle = .zero
     @State private var isNavigating = false
+    @State private var carLocationExists = false
+    @State private var savedCarIcon: String?
+    @State private var referenceText: String?
     
     let icons = (1...16).map { "car\($0)" }
 
     var body: some View {
         NavigationView {
             VStack {
+                Spacer()
+                
                 ZStack {
                     CircularIconPicker(icons: icons, selectedIcon: $selectedIcon, angle: $angle)
                         .frame(width: 300, height: 300)
+                        .padding()
                     
                     if let selectedIconImage = UIImage(named: selectedIcon) {
                         Image(uiImage: selectedIconImage)
                             .resizable()
                             .frame(width: 100, height: 100)
-                            .position(x: UIScreen.main.bounds.width / 2 - 30, y: UIScreen.main.bounds.height/3)
                             .animation(.easeInOut)
                     }
                 }
-                .padding()
+                .frame(maxWidth: .infinity)                
+                Spacer()
+                
+                if carLocationExists, let savedCarIcon = savedCarIcon, let savedCarImage = UIImage(named: savedCarIcon) {
+                    Button(action: {
+                        openMaps()
+                    }) {
+                        HStack {
+                            Image(uiImage: savedCarImage)
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            Text("Go to my car")
+                                .foregroundStyle(.white)
+                        }
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(5)
+                        .padding()
+                        .contextMenu {
+                            Button(action: {
+                                if let referenceText = referenceText {
+                                    showAlertPopup(message: referenceText)
+                                }
+                            }) {
+                                Text(referenceText ?? "")
+                                Image(systemName: "text.bubble")
+                            }
+                        }
+                    }
+                }
                 
                 NavigationLink(destination: saveLocation(carIcon: selectedIcon), isActive: $isNavigating) {
                     Button(action: {
@@ -47,12 +81,14 @@ struct carPicker: View {
                 }
             }
             .padding()
-            .background(backgroudColor)
+            .background(backgroundColor.ignoresSafeArea())
             .navigationTitle("Car Model")
+            .onAppear(perform: checkCarLocation)
         }
     }
     
-    var backgroudColor: Color {
+    //MARK: backgroundColor
+    var backgroundColor: Color {
         switch colorScheme {
         case .light:
             return Color.yellow.opacity(0.15)
@@ -60,6 +96,45 @@ struct carPicker: View {
             return Color(#colorLiteral(red: 0, green: 0, blue: 0.2470588235, alpha: 1))
         @unknown default:
             return Color.yellow.opacity(0.5)
+        }
+    }
+    
+    //MARK: showAlertPopup
+    // Function to show a popup alert with the given message
+    private func showAlertPopup(message: String) {
+        let alert = UIAlertController(title: "Reference", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: checkCarLocation
+    // Function to check if car location exists in UserDefaults
+    private func checkCarLocation() {
+        let latitude = UserDefaults.standard.double(forKey: "latitude")
+        let longitude = UserDefaults.standard.double(forKey: "longitude")
+        
+        carLocationExists = (latitude != 0 && longitude != 0)
+        
+        if let savedReference = UserDefaults.standard.string(forKey: "referenceText") {
+            referenceText = savedReference
+        }
+        
+        if let savedIcon = UserDefaults.standard.string(forKey: "carModel") {
+            savedCarIcon = savedIcon
+        }
+    }
+    
+    //MARK: openMaps
+    // Function to open Apple Maps with saved location
+    private func openMaps() {
+        let latitude = UserDefaults.standard.double(forKey: "latitude")
+        let longitude = UserDefaults.standard.double(forKey: "longitude")
+        
+        if latitude != 0 && longitude != 0 {
+            let urlString = "http://maps.apple.com/?ll=\(latitude),\(longitude)&q=MyCar"
+            if let url = URL(string: urlString) {
+                UIApplication.shared.open(url)
+            }
         }
     }
 }
